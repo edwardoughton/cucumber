a7e847473689ade8dea9cd6bb0b468a6fed2e275
edwardoughton <edward.oughton@gmail.com>
2021-05-13 08:22:38 +0100

    Revise selected files

--- scripts/collect_data.py
+++ scripts/collect_data.py
@@ -34,73 +34,121 @@ def grep_folder():
     directory = os.path.join(
         DATA_RAW,
         'Data Request (GIS)',
-        'Network info (Existing)',
     )
 
     folders = [
         os.path.join(DATA_INTERMEDIATE, 'Mobile Broadband'),
         os.path.join(DATA_INTERMEDIATE, 'Fiber Optic Backbone'),
-        os.path.join(DATA_INTERMEDIATE, 'Fixed Broadband'),
+        # os.path.join(DATA_INTERMEDIATE, 'Fixed Broadband'),
+
     ]
 
     for folder in folders:
         if not os.path.exists(folder):
             os.makedirs(folder)
 
-    file_types = [
-        '.shp',
-        '.gdb'
+    excl_files = [
+        'dbf', '.prj', '.cpg', '.shx'#, ''
+        # '', '.gdbtablx', '.zip', '.gdbtable', '.freelist', '.gdbindexes',
+        # '.idx', '.xlsx', '.CPG', '.qpj', '.spx', '.lyr', '.mxd', '.KMZ',
+        # '.mpk', '.atx', '.lock', '.sbx', '.kmz', '.rar'
+        ]
+
+    files_included = set()
+    files_not_included = set()
+    file_extensions_excluded = set()
+
+    exclusions = [
+        # 'MMOO',
+        # '138',
+        # 'CHILE Division Regional',
+        # 'Enlaces_MMOO_2019_kmz',
+        # 'Nodos_oficio_138_emailv2',
     ]
 
-    exlusions = [
-        'MMOO',
-        '138',
-        'CHILE Division Regional',
-        'Enlaces_MMOO_2019_kmz',
-        'Nodos_oficio_138_emailv2',
-    ]
 
-    for file_type in file_types:
+    for path in Path(directory).rglob('*'):
 
-        for folder in folders:
+        if 'mobile' in str(path).lower():
+            folder = 'Mobile Broadband'
+        elif 'fiber' in str(path).lower():
+            folder = 'Fiber Optic Backbone'
+        else:
+            folder = 'unknown'
 
-            network_type = os.path.basename(folder)
+        file_extension = os.path.splitext(path)[1].lower()
+        file_name = os.path.basename(path)
 
-            search_path = os.path.join(directory, network_type)
+        # if not file_name == 'Of Cir 138-C-2018 ESA FO.kml':
+        #     continue
 
-            for path in Path(search_path).rglob('*{}'.format(file_type)):
+        if any(excp in file_name for excp in exclusions):
+                continue
 
-                file_name = os.path.basename(path)
+        if file_extension == '.gdb':
+            layers = fiona.listlayers(path)
+            for layer in layers:
+                gdf = gpd.read_file(path, layer=layer)
+                filename = '{}_{}'.format(
+                    os.path.basename(os.path.splitext(path)[0]),
+                    layer
+                )
+                path_out = os.path.join(DATA_INTERMEDIATE, folder, filename + '.shp')
+                gdf.to_file(path_out, crs='epsg:4326')
+                files_included.add(file_name)
+                continue
 
-                # if not file_name == 'Claro_KML_Enlaces_of138.gdb':
-                #     continue
+        elif file_extension == '.kml':
 
-                if any(excp in file_name for excp in exlusions):
-                     continue
+            gpd.io.file.fiona.drvsupport.supported_drivers['KML'] = 'rw'
 
-                if file_type == '.gdb':
-                    layers = fiona.listlayers(path)
-                    for layer in layers:
-                        gdf = gpd.read_file(path, layer=layer)
-                        filename = '{}_{}'.format(
-                            os.path.basename(os.path.splitext(path)[0]),
-                            layer
-                        )
-                        path_out = os.path.join(folder, filename + '.shp')
-                        gdf.to_file(path_out, crs='epsg:4326')
-                        continue
+            try:
+                gdf = gpd.read_file(path, driver='KML')
+            except:
+                files_not_included.add(file_name)
+                continue
 
-                try:
-                    ext = os.path.basename(path)[-4:]
-                    filename = os.path.basename(path)[:-4]
-                    gdf = gpd.read_file(path, crs='epsg:4326')
-                    path_out = os.path.join(folder,  filename + ext)
-                    gdf.to_file(path_out, crs='epsg:4326')
+            if 'Point' in [i for i in gdf['geometry'].geom_type]:
+                points = gdf.loc[gdf['geometry'].geom_type == 'Point']
+                path_out = os.path.join(DATA_INTERMEDIATE, folder, file_name + '_points' + '.shp')
+                points.to_file(path_out, crs='epsg:4326')
+                files_included.add(file_name)
+            elif 'LineString' in [i for i in gdf['geometry'].geom_type]:
+                linestrings = gdf.loc[gdf['geometry'].geom_type == 'LineString']
+                path_out = os.path.join(DATA_INTERMEDIATE, folder, file_name + '_lines' + '.shp')
+                linestrings.to_file(path_out, crs='epsg:4326')
+                files_included.add(file_name)
+            else:
+                files_not_included.add(file_name)
 
-                except:
-                    print('path failed: {}'.format(path))
+        elif file_extension == '.shp':
+            filename = os.path.basename(path)[:-4]
+            try:
+                gdf = gpd.read_file(path, crs='epsg:4326')
+            except:
+                files_not_included.add(file_name)
+                continue
+            path_out = os.path.join(DATA_INTERMEDIATE, folder,  filename + '.shp')
+            gdf.to_file(path_out, crs='epsg:4326')
+            files_included.add(file_name)
 
+        else:
+            # print(any(file_extension for word in excl_files))
+            if file_extension in excl_files:
+                print(file_extension)
+                file_extensions_excluded.add(file_name)
+                continue
+            files_not_included.add(file_name)
+            continue
 
+    output = pd.DataFrame({
+        'files_included': pd.Series(list(files_included)),
+        'files_not_included': pd.Series(list(files_not_included)),
+        'file_extensions_excluded': pd.Series(list(file_extensions_excluded)),
+    })
+
+    path = os.path.join(DATA_INTERMEDIATE, 'file_inclusions_and_exclusions.csv')
+    output.to_csv(path, index=False)
 
 if __name__ == "__main__":
 
