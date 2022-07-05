f03adf22a9038a593fe87f4cba079827661305fa
EdwardOughton <edward.oughton@gmail.com>
2022-07-05 16:33:39 -0400

    Add infa sharing emissions strategies

--- scripts/options.py
+++ scripts/options.py
@@ -8,7 +8,7 @@ Written by Ed Oughton, based on work from the pytal and podis repositories.
 
 April 2021
 
-#strategy is defined based on generation_core_backhaul_sharing_networks_spectrum_tax
+#strategy is defined based on generation_core_backhaul_sharing_networks_spectrum_tax_power
 
 generation: technology generation, so 3G or 4G
 core: type of core data transport network, eg. evolved packet core (4G)
@@ -20,6 +20,7 @@ tax: type of taxation strategy, so baseline, high or low
 
 """
 
+
 def generate_tech_options():
     """
     Generate technology strategy options.
@@ -27,9 +28,9 @@ def generate_tech_options():
     """
     output = []
 
-    scenarios = ['low_20_20_20', 'baseline_20_20_20', 'high_20_20_20',
-                'low_10_10_10', 'baseline_10_10_10', 'high_10_10_10',
-                'low_5_5_5', 'baseline_5_5_5', 'high_5_5_5']
+    scenarios = ['low_50_50_50', 'baseline_50_50_50', 'high_50_50_50',
+                'low_30_30_30', 'baseline_30_30_30', 'high_30_30_30',
+                'low_10_10_10', 'baseline_10_10_10', 'high_10_10_10']
     generation_core_types = ['4G_epc', '5G_nsa'] #'3G_umts',
     backhaul_types = ['wireless', 'fiber']
     sharing_types = ['baseline']
@@ -70,9 +71,9 @@ def generate_business_model_options():
     """
     output = []
 
-    scenarios = ['low_20_20_20', 'baseline_20_20_20', 'high_20_20_20',
-                'low_10_10_10', 'baseline_10_10_10', 'high_10_10_10',
-                'low_5_5_5', 'baseline_5_5_5', 'high_5_5_5']
+    scenarios = ['low_50_50_50', 'baseline_50_50_50', 'high_50_50_50',
+                'low_30_30_30', 'baseline_30_30_30', 'high_30_30_30',
+                'low_10_10_10', 'baseline_10_10_10', 'high_10_10_10']
     generation_core_types = ['4G_epc'] #'3G_umts', , '5G_nsa'
     backhaul_types = ['wireless'] #, 'fiber'
     sharing_types = ['baseline', 'passive', 'active', 'srn']
@@ -113,9 +114,9 @@ def generate_policy_options():
     """
     output = []
 
-    scenarios = ['low_20_20_20', 'baseline_20_20_20', 'high_20_20_20',
-                'low_10_10_10', 'baseline_10_10_10', 'high_10_10_10',
-                'low_5_5_5', 'baseline_5_5_5', 'high_5_5_5']
+    scenarios = ['low_50_50_50', 'baseline_50_50_50', 'high_50_50_50',
+                'low_30_30_30', 'baseline_30_30_30', 'high_30_30_30',
+                'low_10_10_10', 'baseline_10_10_10', 'high_10_10_10']
     generation_core_types = ['4G_epc'] #'3G_umts', , '5G_nsa'
     backhaul_types = ['wireless'] #, 'fiber'
     sharing_types = ['baseline']
@@ -156,9 +157,9 @@ def generate_mixed_options():
     """
     output = []
 
-    scenarios = ['low_20_20_20', 'baseline_20_20_20', 'high_20_20_20',
-                'low_10_10_10', 'baseline_10_10_10', 'high_10_10_10',
-                'low_5_5_5', 'baseline_5_5_5', 'high_5_5_5']
+    scenarios = ['low_50_50_50', 'baseline_50_50_50', 'high_50_50_50',
+                'low_30_30_30', 'baseline_30_30_30', 'high_30_30_30',
+                'low_10_10_10', 'baseline_10_10_10', 'high_10_10_10']
     generation_core_types = ['4G_epc'] #'3G_umts', '5G_nsa'
     backhaul_types = ['wireless', 'fiber']
     sharing_types = ['baseline', 'srn']
@@ -199,10 +200,12 @@ def generate_power_options():
     """
     output = []
 
-    scenarios = ['low_20_20_20', 'baseline_20_20_20', 'high_20_20_20',
-                'low_10_10_10', 'baseline_10_10_10', 'high_10_10_10',
-                'low_5_5_5', 'baseline_5_5_5', 'high_5_5_5']
-    generation_core_types = ['4G_epc', '5G_nsa'] #'3G_umts',
+    scenarios = [
+        'low_50_50_50', 'baseline_50_50_50', 'high_50_50_50',
+        'low_30_30_30','baseline_30_30_30','high_30_30_30',
+        'low_10_10_10', 'baseline_10_10_10', 'high_10_10_10'
+        ]
+    generation_core_types = ['4G_epc', '5G_nsa']
     backhaul_types = ['wireless', 'fiber']
     sharing_types = ['baseline']
     networks_types = ['baseline']
@@ -235,12 +238,58 @@ def generate_power_options():
     return output
 
 
+def generate_shared_power_options():
+    """
+    Generate energy strategy options.
+
+    """
+    output = []
+
+    scenarios = [
+        'low_50_50_50', 'baseline_50_50_50', 'high_50_50_50',
+        'low_30_30_30','baseline_30_30_30','high_30_30_30',
+        'low_10_10_10', 'baseline_10_10_10', 'high_10_10_10'
+    ]
+    generation_core_types = ['4G_epc', '5G_nsa'] #'3G_umts',
+    backhaul_types = ['wireless', 'fiber']
+    sharing_types = ['baseline', 'passive', 'active', 'srn']
+    networks_types = ['baseline']
+    spectrum_types = ['baseline']
+    tax_types = ['baseline']
+    power_types = ['baseline']#, 'renewable']
+
+    for scenario in scenarios:
+        for generation_core_type in generation_core_types:
+                for backhaul in backhaul_types:
+                    for sharing in sharing_types:
+                        for network in networks_types:
+                            for spectrum in spectrum_types:
+                                for tax in tax_types:
+                                    for power in power_types:
+                                        strategy = '{}_{}_{}_{}_{}_{}_{}'.format(
+                                            generation_core_type,
+                                            backhaul,
+                                            sharing,
+                                            network,
+                                            spectrum,
+                                            tax,
+                                            power
+                                        )
+                                        output.append({
+                                            'scenario': scenario,
+                                            'strategy': strategy
+                                        })
+
+    return output
+
+
 OPTIONS = {
     'technology_options': generate_tech_options(),
     'business_model_options': generate_business_model_options(),
     'policy_options': generate_policy_options(),
     'mixed_options': generate_mixed_options(),
     'power_options': generate_power_options(),
+    'generate_shared_power_options': generate_shared_power_options(),
 }
 
 
@@ -268,7 +317,8 @@ COSTS = {
 
 
 GLOBAL_PARAMETERS = {
-    'overbooking_factor': 20,
+    'traffic_in_the_busy_hour_perc': 20,
+    # 'overbooking_factor': 20,
     'return_period': 10,
     'discount_rate': 5,
     'opex_percentage_of_capex': 10,
@@ -328,8 +378,8 @@ COST_TYPES = {
 ENERGY_DEMAND = {
     #all costs in $USD
     'equipment_kwh': 5,
-    'core_node_kwh': 10,
-    'regional_node_kwh': 5,
+    'core_node_kwh': 0,
+    'regional_node_kwh': 0,
     'wireless_small_kwh': 5, #Faruk et al. 2019
     'wireless_medium_kwh': 10,
     'wireless_large_kwh': 15,
